b516ca2cd982dab74f4fea9122152729598782a9
EnChang <59942024@chcc.edu.ph>
2026-08-18 18:38:24 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()]
+  plugins: [vue()],
+  base: '/tanglao-module7-vue-system/', // ✅ MUST MATCH YOUR REPO NAME!
 })
